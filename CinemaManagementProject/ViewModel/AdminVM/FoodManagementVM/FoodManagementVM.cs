46e2125ce6d70d0390ffe83b4fcfe28894a0b398
21522402 <21522402@gm.uit.edu.vn>
2022-12-02 14:23:01 +0700

    add navigation staff, how to use

---
+++ CinemaManagementProject/ViewModel/AdminVM/FoodManagementVM/FoodManagementVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaManagementProject.ViewModel.AdminVM.FoodManagementVM
+{
+    class FoodManagementVM:ViewModel.BaseViewModel
+    {
+        public FoodManagementVM()
+        {
+
+        }
+    }
+}
