095e2b9c9ab914131ea58792902120449b3d842c
Fatitboo <21522448@gm.uit.edu.vn>
2022-12-04 10:16:04 +0700

    add View Phat

---
+++ CinemaManagementProject/ViewModel/AdminVM/CustomerManagementVM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaManagementProject.ViewModel.AdminVM
+{
+    internal class CustomerManagementVM: BaseViewModel
+    {
+
+        //private ObservableCollection<CustomerDTO> _customerList;
+        //public ObservableCollection<CustomerDTO> CustomerList
+        //{
+
+        //    get => _customerList;
+        //    set
+        //    {
+        //        _customerList = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
+        //private CustomerDTO _SelectedItem;
+        //public CustomerDTO SelectedItem
+        //{
+        //    get { return _SelectedItem; }
+        //    set { _SelectedItem = value; OnPropertyChanged(); }
+        }
+    }
+}
