33827da461624ec2dd59f465dc3716e595c62d36
Fatitboo <21522448@gm.uit.edu.vn>
2022-12-17 09:02:52 +0700

    add nhe nhe

---
+++ CinemaManagementProject/DTOs/TroubleDTO.cs
@@ -0,0 +1,23 @@
+﻿using CinemaManagementProject.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaManagementProject.DTOs
+{
+    public class TroubleDTO
+    {
+        public int Id { get; set; }
+        public string TroubleType { get; set; }
+        public string TroubleInfo { get; set; }
+        public DateTime StartDate { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public string TroubleStatus { get; set; }
+        public int StaffId { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public virtual Staff Staff { get; set; }
+    }
+}
