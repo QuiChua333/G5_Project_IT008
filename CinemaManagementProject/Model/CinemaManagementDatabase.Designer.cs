d2b62d15af88f7626523fcb9f376e295de1c2706
21522402 <21522402@gm.uit.edu.vn>
2022-12-25 23:10:29 +0700

    change entity

--- CinemaManagementProject/Model/CinemaManagementDatabase.Designer.cs
+++ CinemaManagementProject/Model/CinemaManagementDatabase.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\code_Visual\DO_AN_CHINH\G5_Project_IT008\CinemaManagementProject\Model\CinemaManagementDatabase.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\NhanLe\Desktop\Group5Final2\G5_Project_IT008\CinemaManagementProject\Model\CinemaManagementDatabase.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
