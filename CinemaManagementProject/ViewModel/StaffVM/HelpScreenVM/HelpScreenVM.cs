6d23afedfc9cf2fc363288d47690c0977e4e314c
21522683 <21522683@gm.uit.edu.vn>
2023-02-01 20:11:42 +0700

    Tinh update hyperlink

--- CinemaManagementProject/ViewModel/StaffVM/HelpScreenVM/HelpScreenVM.cs
+++ CinemaManagementProject/ViewModel/StaffVM/HelpScreenVM/HelpScreenVM.cs
@@ -1,10 +1,14 @@
-﻿using CinemaManagementProject.View.Staff.HelpScreen;
+﻿using CinemaManagementProject.DTOs;
+using CinemaManagementProject.View.Admin.HelpScreen;
+using CinemaManagementProject.View.Admin.MovieManagement;
+using CinemaManagementProject.View.Login;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace CinemaManagementProject.ViewModel.StaffVM.HelpScreenVM
@@ -17,27 +21,27 @@ namespace CinemaManagementProject.ViewModel.StaffVM.HelpScreenVM
         public ICommand Load_Frequently_asked_questions { get; set; }
         public ICommand Load_Privacy_Policy { get; set; }
         public ICommand FB_Command { get; set; }
-        public ICommand Zalo_Command { get; set; }
-        public ICommand Twitter_Command { get; set; }
-        public ICommand Instagram_Command { get; set; }
+        public ICommand FB_Nhan_Command { get; set; }
+        public ICommand FB_Qui_Command { get; set; }
+        public ICommand FB_Phat_Command { get; set; }
         public HelpScreenVM()
         {
-            Instagram_Command = new RelayCommand<object>((uri) => { return true; }, (uri) =>
-            {
-                string myUri = !uri.ToString().Contains("https://") && !uri.ToString().Contains("http://") ? "http://" + uri.ToString() : uri.ToString();
-                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(myUri));
-            });
-            Twitter_Command = new RelayCommand<object>((uri) => { return true; }, (uri) =>
-            {
-                string myUri = !uri.ToString().Contains("https://") && !uri.ToString().Contains("http://") ? "http://" + uri.ToString() : uri.ToString();
-                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(myUri));
-            });
-            Zalo_Command = new RelayCommand<object>((uri) => { return true; }, (uri) =>
-            {
-                string myUri = !uri.ToString().Contains("https://") && !uri.ToString().Contains("http://") ? "http://" + uri.ToString() : uri.ToString();
-                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(myUri));
-            });
             FB_Command = new RelayCommand<object>((uri) => { return true; }, (uri) =>
+            {
+                string myUri = !uri.ToString().Contains("https://") && !uri.ToString().Contains("http://") ? "http://" + uri.ToString() : uri.ToString();
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(myUri));
+            });
+            FB_Nhan_Command = new RelayCommand<object>((uri) => { return true; }, (uri) =>
+            {
+                string myUri = !uri.ToString().Contains("https://") && !uri.ToString().Contains("http://") ? "http://" + uri.ToString() : uri.ToString();
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(myUri));
+            });
+            FB_Qui_Command = new RelayCommand<object>((uri) => { return true; }, (uri) =>
+            {
+                string myUri = !uri.ToString().Contains("https://") && !uri.ToString().Contains("http://") ? "http://" + uri.ToString() : uri.ToString();
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(myUri));
+            });
+            FB_Phat_Command = new RelayCommand<object>((uri) => { return true; }, (uri) =>
             {
                 string myUri = !uri.ToString().Contains("https://") && !uri.ToString().Contains("http://") ? "http://" + uri.ToString() : uri.ToString();
                 System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(myUri));
